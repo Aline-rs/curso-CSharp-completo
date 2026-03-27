8cdaa51906ecfdf6498b3675a1380d3135b2ab65
Aline Rosa <alinerosa_s@hotmail.com>
2026-03-27 19:05:22 -0300

    Resolvendo exercicio 1 - ExerciciosEstruturaSequencia

---
+++ cSharp-Projeto/Program.cs
@@ -0,0 +1,17 @@
+﻿using cSharp_Projeto.Sessão_3.Exercicios_logica;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var exerciciosEstruturaSequencial = new ExerciciosEstruturaSequencial();
+
+        int primeiroNumero = exerciciosEstruturaSequencial.LerNumero("Digite o primeiro número: ");
+        int segundoNumero = exerciciosEstruturaSequencial.LerNumero("Digite o segundo número: ");
+
+        int resultado = exerciciosEstruturaSequencial.Somar(primeiroNumero, segundoNumero);
+
+        exerciciosEstruturaSequencial.ExibirResultado(resultado);
+        Console.ReadLine();
+    }
+}
---
+++ cSharp-Projeto/Sessão 3/Exercicios-logica/exercicios1-estrutura-sequencial.cs
@@ -0,0 +1,22 @@
+﻿namespace cSharp_Projeto.Sessão_3.Exercicios_logica
+{
+    public class ExerciciosEstruturaSequencial
+    {
+        public int Somar(int primeiroNumero, int segundoNumero)
+        {
+            return primeiroNumero + segundoNumero;
+        }
+
+        public int LerNumero(string mensagem)
+        {
+            Console.WriteLine(mensagem);
+
+            return int.Parse(Console.ReadLine());
+        }
+
+        public void ExibirResultado(int resultado)
+        {
+            Console.WriteLine($"SOMA = {resultado}");
+        }
+    }
+}
