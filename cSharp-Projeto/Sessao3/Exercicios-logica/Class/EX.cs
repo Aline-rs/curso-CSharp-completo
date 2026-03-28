27494f68da65a46f4d545e783bea07121c50bd1d
Aline Rosa <alinerosa_s@hotmail.com>
2026-03-28 14:36:36 -0300

    Entendendo o conceito de herença vs composição e realizando alteração.

--- cSharp-Projeto/Sessao3/Exercicios-logica/Class/EX.cs
+++ cSharp-Projeto/Sessao3/Exercicios-logica/Class/EX.cs
@@ -1,101 +1,108 @@
 ﻿namespace cSharp_Projeto.Sessão_3.Exercicios_logica.Class
 {
-    public class EX : ExerciciosEstruturaSequencial
+    public class EX
     {
+        private readonly ExerciciosEstruturaSequencial _utils;
+
+        public EX()
+        {
+            _utils = new ExerciciosEstruturaSequencial();
+        }
+
         public void Ex1()
         {
-            int primeiroNumero = LerInteiro("Digite o primeiro número: ");
-            int segundoNumero = LerInteiro("Digite o segundo número: ");
+            int primeiroNumero = _utils.LerInteiro("Digite o primeiro número: ");
+            int segundoNumero = _utils.LerInteiro("Digite o segundo número: ");
 
-            int resultado = Somar(primeiroNumero, segundoNumero);
+            int resultado = _utils.Somar(primeiroNumero, segundoNumero);
 
             Console.Clear();
 
-            ExibirResultado("SOMA = ", resultado, "F0");
+            _utils.ExibirResultado("SOMA = ", resultado, "F0");
             Console.ReadLine();
         }
 
         public void Ex2()
         {
-            double raio = LerDecimal("Digite o valor do raio: ");
-            double area = CalcularAreaDoCirculo(raio);
+            double raio = _utils.LerDecimal("Digite o valor do raio: ");
+            double area = _utils.CalcularAreaDoCirculo(raio);
 
             double resultado = area;
 
             Console.Clear();
 
-            ExibirResultado("A=", resultado, "F4");
+            _utils.ExibirResultado("A=", resultado, "F4");
             Console.ReadLine();
         }
 
         public void EX3()
         {
-            int a = LerInteiro("Digite o valor de A: ");
-            int b = LerInteiro("Digite o valor de B: ");
-            int c = LerInteiro("Digite o valor de C: ");
-            int d = LerInteiro("Digite o valor de D: ");
+            int a = _utils.LerInteiro("Digite o valor de A: ");
+            int b = _utils.LerInteiro("Digite o valor de B: ");
+            int c = _utils.LerInteiro("Digite o valor de C: ");
+            int d = _utils.LerInteiro("Digite o valor de D: ");
 
-            int resultado = CalcularDiferenca(a, b, c, d);
+            int resultado = _utils.CalcularDiferenca(a, b, c, d);
 
             Console.Clear();
 
-            ExibirResultado("DIFERENÇA = ", resultado, "F0");
+            _utils.ExibirResultado("DIFERENÇA = ", resultado, "F0");
             Console.ReadLine();
         }
 
         public void EX4()
         {
 
-            int numeroFuncionario = LerInteiro("Digite o número do funcionário: ");
-            double horasTrabalhadas = LerDecimal("Digite as horas trabalhadas: ");
-            double valorHora = LerDecimal("Digite o valor da hora: ");
+            int numeroFuncionario = _utils.LerInteiro("Digite o número do funcionário: ");
+            double horasTrabalhadas = _utils.LerDecimal("Digite as horas trabalhadas: ");
+            double valorHora = _utils.LerDecimal("Digite o valor da hora: ");
 
-            double resultado = CalcularSalarioHora(valorHora, horasTrabalhadas);
+            double resultado = _utils.CalcularSalarioHora(valorHora, horasTrabalhadas);
 
             Console.Clear();
 
-            ExibirResultado("NUMBER = ", numeroFuncionario, "F0");
-            ExibirResultado("SALARY = U$ ", resultado, "F2");
+            _utils.ExibirResultado("NUMBER = ", numeroFuncionario, "F0");
+            _utils.ExibirResultado("SALARY = U$ ", resultado, "F2");
             Console.ReadLine();
         }
 
         public void EX5()
         {
-            int codigoPeca1 = LerInteiro("Digite o código da peça 1: ");
-            int numeroPeca1 = LerInteiro("Digite o número de peças 1: ");
-            double valorUnitarioPeca1 = LerDecimal("Digite o valor unitário da peça 1: ");
+            int codigoPeca1 = _utils.LerInteiro("Digite o código da peça 1: ");
+            int numeroPeca1 = _utils.LerInteiro("Digite o número de peças 1: ");
+            double valorUnitarioPeca1 = _utils.LerDecimal("Digite o valor unitário da peça 1: ");
 
-            int codigoPeca2 = LerInteiro("Digite o código da peça 2: ");
-            int numeroPeca2 = LerInteiro("Digite o número de peças 2: ");
-            double valorUnitarioPeca2 = LerDecimal("Digite o valor unitário da peça 2: ");
+            int codigoPeca2 = _utils.LerInteiro("Digite o código da peça 2: ");
+            int numeroPeca2 = _utils.LerInteiro("Digite o número de peças 2: ");
+            double valorUnitarioPeca2 = _utils.LerDecimal("Digite o valor unitário da peça 2: ");
 
-            double resultado = CalcularValorTotalPecas(numeroPeca1, valorUnitarioPeca1, numeroPeca2, valorUnitarioPeca2);
+            double resultado = _utils.CalcularValorTotalPecas(numeroPeca1, valorUnitarioPeca1, numeroPeca2, valorUnitarioPeca2);
 
             Console.Clear();
 
-            ExibirResultado("VALOR A PAGAR: R$ ", resultado, "F2");
+            _utils.ExibirResultado("VALOR A PAGAR: R$ ", resultado, "F2");
             Console.ReadLine();
         }
 
         public void EX6()
         {
-            double valorA = LerDecimal("Digite o valor de A: ");
-            double valorB = LerDecimal("Digite o valor de B: ");
-            double valorC = LerDecimal("Digite o valor de C: ");
+            double valorA = _utils.LerDecimal("Digite o valor de A: ");
+            double valorB = _utils.LerDecimal("Digite o valor de B: ");
+            double valorC = _utils.LerDecimal("Digite o valor de C: ");
 
-            double triangulo = CalcularAreaDoTriangulo(valorA, valorC);
-            double circulo = CalcularAreaDoCirculo(valorC);
-            double trapezio = CalcularAreaDoTrapezio(valorA, valorB, valorC);
-            double quadrado = CalcularAreaDoQuadrado(valorB);
-            double retangulo = CalcularAreaDoRetangulo(valorA, valorB);
+            double triangulo = _utils.CalcularAreaDoTriangulo(valorA, valorC);
+            double circulo = _utils.CalcularAreaDoCirculo(valorC);
+            double trapezio = _utils.CalcularAreaDoTrapezio(valorA, valorB, valorC);
+            double quadrado = _utils.CalcularAreaDoQuadrado(valorB);
+            double retangulo = _utils.CalcularAreaDoRetangulo(valorA, valorB);
 
             Console.Clear();
 
-            ExibirResultado("TRIANGULO: ", triangulo, "F3");
-            ExibirResultado("CIRCULO: ", circulo, "F3");
-            ExibirResultado("TRAPEZIO: ", trapezio, "F3");
-            ExibirResultado("QUADRADO: ", quadrado, "F3");
-            ExibirResultado("RETANGULO: ", retangulo, "F3");
+            _utils.ExibirResultado("TRIANGULO: ", triangulo, "F3");
+            _utils.ExibirResultado("CIRCULO: ", circulo, "F3");
+            _utils.ExibirResultado("TRAPEZIO: ", trapezio, "F3");
+            _utils.ExibirResultado("QUADRADO: ", quadrado, "F3");
+            _utils.ExibirResultado("RETANGULO: ", retangulo, "F3");
             Console.ReadLine();
         }
     }
